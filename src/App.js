cfe57c6107d2fecc7ff2f93f0fb86d2da6cd5e45
Jose Lara <Jose@Joses-MacBook-Pro.local>
2017-11-22 20:26:02 -0500

    working with event handlers, state, and props

--- src/App.js
+++ src/App.js
@@ -3,15 +3,45 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      {name: 'Jose', age: 28},
+      {name: 'Joe', age: 26},
+      {name: 'Duke', age: 27}
+    ]
+  }
+
+  switchNameHandler = () => {
+    console.log("Was clicked!");
+    this.setState({
+      persons: [
+        {name: 'PEPE', age: 28},
+        {name: 'Joe', age: 26},
+        {name: 'Amigo', age: 27}
+      ]
+    });
+
+  };
+
+  nameChangeHandler = (event) => {
+    this.setState({
+      persons: [
+        {name: event.target.value, age: 28},
+        {name: 'Jaja', age: 26},
+        {name: 'Hey', age: 27}
+      ]
+
+    });
+  };
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm Jose</h1>
-        <Person name="Jose" age="29"/>
-        <Person name="Luis" age="23"/>
-        <Person name="Joe" age="26"/>
-        <Person name="Duke" age="19">I'm Learning React</Person>
-        <Person name="Pepe" age="33"/>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age="29"/>
+        <Person name={this.state.persons[1].name} age="29" changed={this.nameChangeHandler}/>
+        <Person name={this.state.persons[2].name} age="29"/>
       </div>
 
     );
