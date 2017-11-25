33b9de06aa9fdc53b6421e8be359f03f4b9dc229
Jose Lara <Jose@Joses-MacBook-Pro.local>
2017-11-24 20:58:33 -0500

    changing list to be dynamic

--- src/App.js
+++ src/App.js
@@ -5,32 +5,49 @@ import Person from './Person/Person';
 class App extends Component {
   state = {
     persons: [
-      {name: 'Jose', age: 28},
-      {name: 'Joe', age: 26},
-      {name: 'Duke', age: 27}
-    ]
-  }
+      {id: '12314dd', name: 'Jose', age: 28},
+      {id: '13ddsf', name: 'Joe', age: 26},
+      {id: '332rrs', name: 'Duke', age: 27}
+    ],
+    showPerson: false
+  };
 
-  switchNameHandler = () => {
-    this.setState({
-      persons: [
-        {name: 'PEPE', age: 28},
-        {name: 'Joe', age: 26},
-        {name: 'Amigo', age: 3}
-      ]
+  changeNameHandler = (event, id) => {
+    // find the index in the list
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
     });
+
+    // get the person and assign a copy to a new object
+    // this is the same as const person = Object.assign({}, this.state.persons[personIndex]);
+    const person = {...this.state.persons[personIndex]};
+
+    //get value from event
+    person.name = event.target.value;
+
+    // copy the entire list to be updated
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({persons: persons});
 
   };
 
-  nameChangeHandler = (event) => {
-    this.setState({
-      persons: [
-        {name: event.target.value, age: 28},
-        {name: event.target.value, age: 26},
-        {name: event.target.value, age: 27}
-      ]
+  deleteNameHandler = (index) => {
+    // don't use this because you are changing items by reference
+    //const persons = this.state.persons;
+    // use this intead
+    const persons = [...this.state.persons];
+    //or use
+    //const persons = this.state.persons.slice();
+    persons.splice(index, 1);
+    this.setState({persons: persons})
+    
+  };
 
-    });
+  showPersonHandler = () =>{
+    const doesShow = this.state.showPerson;
+    this.setState({showPerson: !doesShow})
   };
 
   render() {
@@ -44,13 +61,29 @@ class App extends Component {
       cursor:'pointer'
     };
 
+    let persons = null;
+
+    if(this.state.showPerson){
+        persons = (
+        <div>
+          {this.state.persons.map((person, index) => {
+            return <Person name={person.name} 
+                    age={person.age} 
+                    key={person.id}
+                    click={this.deleteNameHandler.bind(this, index)}
+                    changed={(event)=>this.changeNameHandler(event, person.id)}
+                    />
+          })}
+        </div>
+      );
+    }
+
+
     return (
       <div className="App">
         <h1>Hi, I'm Jose</h1>
-        <button style={style} onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} changed={this.nameChangeHandler}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} changed={this.nameChangeHandler}/>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} changed={this.nameChangeHandler}/>
+        <button style={style} onClick={this.showPersonHandler}>Show Names</button>
+        {persons}
       </div>
 
     );
